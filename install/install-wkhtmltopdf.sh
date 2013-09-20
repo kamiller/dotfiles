67a966439650444c30a3a0b1c3356fbd63b07819
Kyle <github@kylemiller.com>
2013-09-20 13:59:34 +0000

    updated to work properly

--- install/install-wkhtmltopdf.sh
+++ install/install-wkhtmltopdf.sh
@@ -1,11 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
 #ubuntu 12.04 x64bit
 
 
-wget https://code.google.com/p/wkhtmltopdf/downloads/detail?name=wkhtmltopdf-0.9.9-static-amd64.tar.bz2&can=2&q=
+wget https://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-amd64.tar.bz2
 tar xvjf wkhtmltopdf-0.9.9-static-amd64.tar.bz2 
-sudo mv wkhtmltopdf-0.9.9-static-amd64 /usr/local/bin/wkhtmltopdf
+sudo mv wkhtmltopdf-amd64 /usr/local/bin/wkhtmltopdf
 
 #install dependencies
 sudo apt-get install openssl build-essential xorg libssl-dev libxrender-dev -y
